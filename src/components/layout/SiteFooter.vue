c5fc52243b6acda4c4f3c00d656e94a824e0d252
Bhushan  Maheshwari <bhushan.maheshwari@gmail.com>
2022-11-22 23:16:24 -0500

    updated home page content

---
+++ src/components/layout/SiteFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Developed</strong> by
+        <a href="https://linkedin.com/in/bhushanmaheshwari" target="_blank"
+          >Bhushan Maheshwari</a
+        >. The source code is licensed
+        <a href="http://opensource.org/licenses/mit-license.php" target="_blank"
+          >MIT</a
+        >. The website content is licensed
+        <a
+          href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
+          target="_blank"
+          >CC BY NC SA 4.0</a
+        >.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,31 @@
 <script>
 import BaseButton from "../components/ui/BaseButton.vue";
+import { ref } from "vue";
+import { useStore } from "vuex";
 export default {
   components: { BaseButton },
+
   setup() {
-    const strategize = {
+    const store = useStore();
+    const task1 = store.getters["tasks/task1"];
+    const task2 = store.getters["tasks/task2"];
+
+    const strategize = ref({
+      title: task1.title,
+      subtitle: task1.description,
       text: "Strategize",
       to: "/strategy/need",
-    };
-    const worksmarter = {
+    });
+
+    const worksmarter = ref({
+      title: task2.title,
+      subtitle: task2.description,
       text: "Work Smarter",
       to: "/worksmarter",
-    };
+    });
+
     const about = {
-      text: "About",
+      text: "If yes, click here!",
       to: "/about",
     };
     return {
@@ -27,38 +40,29 @@ export default {
 
 <template>
   <section>
+    <section class="hero is-large has-text-info is-white">
+      <div class="hero-body welcome-text">
+        <p class="title is-1">Hello, Good morning!</p>
+        <p class="subtitle is-2">Things we are going to dicuss today.</p>
+      </div>
+    </section>
+
     <section class="section">
       <div class="tile is-ancestor">
-        <div class="tile is-parent is-4">
-          <article class="tile is-child box">
-            <p class="title">One</p>
-            <p class="subtitle">Subtitle</p>
-          </article>
-        </div>
         <div class="tile is-parent">
           <article class="tile is-child box">
-            <p class="title">Two</p>
-            <p class="subtitle">Subtitle</p>
+            <p class="title">{{ strategize.title }}</p>
+            <p class="subtitle">{{ strategize.subtitle }}</p>
             <base-button
               :text="strategize.text"
               :to="strategize.to"
             ></base-button>
           </article>
         </div>
-      </div>
-    </section>
-    <section class="section">
-      <div class="tile is-ancestor">
-        <div class="tile is-parent is-4">
-          <article class="tile is-child box">
-            <p class="title">One</p>
-            <p class="subtitle">Subtitle</p>
-          </article>
-        </div>
         <div class="tile is-parent">
           <article class="tile is-child box">
-            <p class="title">Two</p>
-            <p class="subtitle">Subtitle</p>
+            <p class="title">{{ worksmarter.title }}</p>
+            <p class="subtitle">{{ worksmarter.subtitle }}</p>
             <base-button
               :text="worksmarter.text"
               :to="worksmarter.to"
@@ -67,21 +71,15 @@ export default {
         </div>
       </div>
     </section>
-    <section class="section">
-      <div class="tile is-ancestor">
-        <div class="tile is-parent is-4">
-          <article class="tile is-child box">
-            <p class="title">One</p>
-            <p class="subtitle">Subtitle</p>
-          </article>
-        </div>
-        <div class="tile is-parent">
-          <article class="tile is-child box">
-            <p class="title">Two</p>
-            <p class="subtitle">Subtitle</p>
-            <base-button :text="about.text" :to="about.to"></base-button>
-          </article>
-        </div>
+
+    <section class="hero is-medium is-light">
+      <div class="hero-body">
+        <p class="title">How this website works?</p>
+        <p class="subtitle">
+          Are you interested in knowing how this website is working behind the
+          scenes?
+        </p>
+        <base-button :text="about.text" :to="about.to"></base-button>
       </div>
     </section>
   </section>
