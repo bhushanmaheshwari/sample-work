00d80819e06fab30cb7d018f89a8462225b45d81
Bhushan  Maheshwari <bhushan.maheshwari@gmail.com>
2022-11-24 19:32:41 -0500

    updated footer and pdf

--- src/components/layout/SiteFooter.vue
+++ src/components/layout/SiteFooter.vue
@@ -3,9 +3,9 @@
     <div class="hero-body has-text-centered">
       <p>
         <strong>Developed</strong> by
-        <a href="https://linkedin.com/in/bhushanmaheshwari" target="_blank"
+        <a class="has-text-link" href="https://linkedin.com/in/bhushanmaheshwari" target="_blank"
           >Bhushan Maheshwari</a
-        >.
+        >
       </p>
       <p>Software Architect | MBA | JavaScript | Python | Azure</p>
     </div>
