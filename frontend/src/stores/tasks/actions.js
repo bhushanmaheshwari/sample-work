53f5935edee797709410bc02847c91abad9c7847
Bhushan  Maheshwari <bhushan.maheshwari@gmail.com>
2022-11-26 21:56:53 -0500

    added frontend changes

--- frontend/src/stores/tasks/actions.js
+++ frontend/src/stores/tasks/actions.js
@@ -1,8 +1,16 @@
+
 export default {
     setBc(context, bc) {
         context.commit('setBreadcrumbs', bc);
     },
     setHomePage(context, isHomePage) {
         context.commit('setHomePage', isHomePage);
+    },
+    getData(context, isHomePage) {
+        fetch('https://api-sample-work.azurewebsites.net/api/coaches', {
+            method: 'GET'
+        }).then((data) => {
+            console.log(data);
+        })
     }
 };
