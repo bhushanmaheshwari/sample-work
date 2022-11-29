af5101cd5b5bf2bbcd69bbc5490163ad1cab710b
Bhushan  Maheshwari <bhushan.maheshwari@gmail.com>
2022-11-29 18:19:03 -0500

    integrated base version of google analytics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,8 +10,26 @@ import BaseDeepdive from './components/ui/BaseDeepdive.vue';
 import BaseTitle from './components/ui/BaseTitle.vue';
 import BaseBreadcrumbs from './components/ui/BaseBreadcrumbs.vue'
 
-const app = createApp(App)
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics, logEvent } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDK2eQQPzImlDlvpRKQVuIjz7I0KWjLXo",
+  authDomain: "bm-sample-work.firebaseapp.com",
+  projectId: "bm-sample-work",
+  storageBucket: "bm-sample-work.appspot.com",
+  messagingSenderId: "373331721738",
+  appId: "1:373331721738:web:e09e1080d5886cd42da9f3",
+  measurementId: "G-K02RXPSNZ7"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+logEvent(analytics, 'app_loaded');
+
+const app = createApp(App)
 
 app.use(router);
 app.use(store);
